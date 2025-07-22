b89712f5f4e48fdf8aa8373a92eb4090e4970120
srinivasanhck <srinivasan@cleankoding.com>
2025-07-22 19:40:23 +0530

    commited Personal Information Collector

---
+++ Tue/q1_Personal Information Collector/main.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobbies = input("Enter your hobbies: ")
+
+print(f"Hello {name}! You are {age} years old and you like {hobbies}.s")
